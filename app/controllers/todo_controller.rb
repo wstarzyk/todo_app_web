0f20b2c7d3aeade30ad9e1090a78378b9c2bef64
wstarzk <wstarzyk@gmail.com>
2017-12-30 21:21:48 +0000

    Add the RCAV flows for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,19 @@
 class TodoController < ApplicationController
     def index
     end
+    
     def show
         @todo = Todo.find_by_id(params[:id])
+    end
+    
+    def new
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
     end
 end
