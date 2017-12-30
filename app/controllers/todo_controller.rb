f50f6b8ef59108d1f5dd7efe7bb1a117d9797a4c
wstarzk <wstarzyk@gmail.com>
2017-12-30 02:16:39 +0000

    Add links to todos on the index page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -11,6 +11,27 @@ class TodoController < ApplicationController
         elsif todo_id == '2'
             @todo_description = "Buy workshop supplies"
             @todo_pomodoro_estimate = 3
+            
+        elsif todo_id == '3'
+            @todo_description = "Meet with the volunteer trainers"
+            @todo_pomodoro_estimate = 2
+            
+        elsif todo_id == '4'
+            @todo_description = "Order food for Saturday and Sunday"
+            @todo_pomodoro_estimate = 1
+            
+        elsif todo_id == '5'
+            @todo_description = "Check pre-work assignments"
+            @todo_pomodoro_estimate = 2
+            
+        elsif todo_id == '6'
+            @todo_description = "Send workshop location to all the students"
+            @todo_pomodoro_estimate = 1
+            
+        elsif todo_id == '7'
+            @todo_description = "Have a great workshop"
+            @todo_pomodoro_estimate = 0
+            
         end
     end
 end
